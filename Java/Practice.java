3632b889c70550eb63f8f01063b8c4e59d9f5683
dev yamil <devyamil1998@gmail.com>
2023-02-21 19:30:20 -0300

    POO-Class and constructor

--- Java/Practice.java
+++ Java/Practice.java
@@ -1,38 +1,89 @@
 package Java;
 
-public class Practice {
+import java.util.Scanner;
+
+public class Practice{
+   private String name;
+   private int dni;
+   private int age;
+
+   //Constructror sin parametros
+   public Practice(){};
+
+   //Constructor con parametros
+   public Practice(String name, int dni, int age){
+      this.name = name;
+      this.dni = dni;
+      this.age = age;
+   };
+
+   //Método GET
+   public String getName() {
+      return name;
+   }
+   public int getDni() {
+      return dni;
+   }public int getAge() {
+      return age;
+   }
+
+   //Método SET
+   public void setName(String name) {
+      this.name = name;
+   }
+   public void setDni(int dni) {
+      this.dni = dni;
+   }
+   public void setAge(int age) {
+      this.age = age;
+   }
+
+   public String toString(){
+      String aux = "Hi, my name is " + getName() + ", my dni is " + getDni() + " and have " + getAge() + " years.";
+      return aux;
+   }
+
+
    public static void main(String[] args){
-      /* for(int i = 1; i < 10; i++){
-         System.out.println("Numero " + i);
-      } */ 
+      int dimF = 15;
+      int dimL = 0;
+      Practice []users = new Practice[dimF];
 
-      /* Creación de arreglos: */
-      /*  int [] posiciones;
-      posiciones = new int[7];
-      for (int i = 0; i < posiciones.length; i++) {
-         posiciones[i] = i + 2;
-      }
-      for (int i = 0; i < posiciones.length; i++) {
-         System.out.println(posiciones[i]);
-      } */
+      Scanner read = new Scanner(System.in);
+      String name;
+      int dni, age;
 
+      System.out.print("Enter your name: ");
+      name = read.nextLine();
+      
+      System.out.print("- DNI: ");
+      dni = read.nextInt();
 
-      //Matrices
-      int [][]tabla = new int[2][3];
+      System.out.print("- Age: ");
+      age = read.nextInt();
 
-      for (int f = 0; f < 2; f++) {
-         for (int c = 0; c < 3; c++) {
-            tabla[f][c] = c + f;
-         }
-      }
+      while((dimL < dimF) && (age != 0)){
+         users[dimL] = new Practice(name, dni, age);
+         dimL++;
 
-      for (int f = 0; f < 2; f++) {
-         System.out.print("FILA " + (f+1) + ": ");
-         for (int c = 0; c < 3; c++) {
-            System.out.print(tabla[f][c] + " | ");
-         }
          System.out.println();
+         System.out.print("Enter your name: ");
+         name = read.nextLine();
+   
+         System.out.print("- DNI: ");
+         dni = read.nextInt();
+
+         System.out.print("- Age: ");
+         age = read.nextInt();
       }
+
+      if(dimL > 0){
+         System.out.println("The number of added users is " + dimL);
+      }else{
+         System.out.println("No user added");
+      }
+
+      read.close();
 
 
    }
