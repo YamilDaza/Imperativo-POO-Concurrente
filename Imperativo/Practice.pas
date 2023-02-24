c952cf9cee0fc113254a6d5ac1628e323671b430
dev yamil <devyamil1998@gmail.com>
2023-02-24 18:37:19 -0300

    recursion

--- Imperativo/Practice.pas
+++ Imperativo/Practice.pas
@@ -1,4 +1,12 @@
 program Practice;
+   procedure recursion(x:integer);
+   begin
+      if(x < 5)then begin
+         x:=x-1;
+         recursion(x);
+      end;
+   end;
+
    procedure imprimir(x:integer);
    begin
       if(x > 0)then begin
@@ -12,5 +20,6 @@ var
 begin
    x:=5;
    imprimir(x);
+   recursion(1)
 end.
 
